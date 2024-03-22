81497877937a57be6d9440a7c6f57cf42654edfe
saba-yousf <162787231+saba-yousf@users.noreply.github.com>
2024-03-22 23:54:11 +0800

    Add files via upload

---
+++ 5.Famous_Quote_2.ts
@@ -0,0 +1,7 @@
+import { toASCII } from "punycode";
+
+let famous_person :string ='Albert Einstein';
+
+let message :string ='“A person who never made a mistake never tried anything new"';
+
+console.log(`${famous_person} once said, ${message}`);
