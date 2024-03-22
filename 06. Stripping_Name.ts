4901b9f502944ed9d4844b5c83493ad87b55c9b8
saba-yousf <162787231+saba-yousf@users.noreply.github.com>
2024-03-22 23:54:48 +0800

    Add files via upload

---
+++ 06. Stripping_Name.ts
@@ -0,0 +1,11 @@
+
+
+const personName : string = '    \n\tsaba yousuf\t\n    '; 
+
+
+console.log(personName);
+
+const without_whitespace :string = personName.trim();
+
+console.log(without_whitespace);
+ 
