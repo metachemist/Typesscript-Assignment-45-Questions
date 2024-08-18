b05b2ca5147a5ece6bbf7e1fc7d9460302e02372
metachemist <meta.chemist.22@gmail.com>
2024-08-18 16:48:48 +0500

    Adding all assignments

--- 41. Magicians.ts
+++ 41. Magicians.ts
@@ -1,13 +1,13 @@
-// Magicians: Make a array of magician’s names.
+// Magicians: Make an array of magician’s names.
 // Pass the array to a function called show_magicians(), 
 // which prints the name of each magician in the array.
 
-let magician : string[] = ['harrypotar', 'hermione granger', 'ron weasly', 'albus dumbledore'];
+let magician: string[] = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
 
-function show_magicians(magicians:string[]){
-   magician.forEach(element => {
+function show_magicians(magicians: string[]) {
+   magicians.forEach(element => {
      console.log(element);
    });
 }
 
-show_magicians(magician)
+show_magicians(magician);
